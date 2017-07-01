f9afac9468c278b2d64bc7cd1f593e56839a72cb
JTTran <tranjt80@gmail.com>
2017-07-01 18:27:27 +0300

    Set up separate  test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 
 // "G:\Program Files\MongoDB\Server\3.4.5\bin\mongod.exe" --dbpath "G:\Program Files\MongoDB\data\mongo-data"
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGOLAB_URI);
 
 module.exports =  {
     mongoose
 }
+
+
+// process.env.NODE_ENV === "production"
+// process.env.NODE_ENV === "development"
+// process.env.NODE_ENV === "test"
