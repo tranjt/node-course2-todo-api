ca55c4ae19bd3626ffa9eaf5564713b8edfc970c
JTTran <tranjt80@gmail.com>
2017-07-01 18:01:35 +0300

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
     text: "First test todo"
 }, {
     _id: new ObjectID(),
-    text: "Second test todo"
+    text: "Second test todo",
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -143,6 +145,52 @@ describe("DELETE /todos/:id", () => {
         request(app)
             .delete("/todos/123")
             .expect(404)            
+            .end(done);
+    });
+});
+
+describe("PATCH /todos/:id", () => {
+    it("should update the todo", (done) => {
+        // grab id of first item
+        // update text, set completed true
+        // 200
+        // text is changed, completed is true, completedAt is a number toBeA
+        const hexId = todos[0]._id.toHexString();
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({ 
+                "completed": true, 
+                "text": "test changing text" 
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe("test changing text");
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA("number");
+               
+            })
+            .end(done);
+
+    });
+
+    it("should clear completedAt when todo is not completed", (done) => {
+        // grab id of second todo item 
+        // update text, set completed to false
+        // 200
+        // text is changed, completed is false, completedAt is null toNotExist
+        const hexId = todos[1]._id.toHexString();
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send( {
+                "text": "test changing text", 
+                "completed": false 
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe("test changing text");
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
             .end(done);
     });
 });
