705e7f280aeb48e583ec069fd5b89925ec59f296
JTTran <tranjt80@gmail.com>
2017-07-05 01:14:54 +0300

    Salt and Hash user password before saving with bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-    id: 10
-};
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, "123abc");
-console.log(decoded);
+var hashedPassword = "$2a$10$Izxjlz4eyaitfW8mbT46x.bkxOSAvM2XN82G8oJLI02VOA89eQ90m";
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+
+// var decoded = jwt.verify(token, "123abc");
+// console.log(decoded);
 
 // jwt.verify
 
