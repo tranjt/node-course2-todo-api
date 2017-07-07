e034d3b5afe67c1718378c0c7d4982aaef979cc5
JTTran <tranjt80@gmail.com>
2017-07-07 18:29:00 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -57,6 +57,18 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+    
+};
+
 // custom model method
 UserSchema.statics.findByToken = function (token) {
     // "this" is User instead of user
@@ -102,6 +114,8 @@ UserSchema.statics.findByCredentials = function(email, password) {
         });
     });
 };
+
+
 
 
 // salt and hash password before save to database using bcryptjs
--- server/server.js
+++ server/server.js
@@ -134,6 +134,14 @@ app.post("/users/login", (req, res) => {
     });    
 });
     
+app.delete("/users/me/token", authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }).catch((error) => {
+        res.status(400).send();
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Start up at port ${port}`);
